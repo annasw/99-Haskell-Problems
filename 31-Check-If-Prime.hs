a741ed2759b9d1df508578515b010b6039a310f4
Jesse White <jsperlingwhite@gmail.com>
2017-04-27 01:52:35 -0700

    Create 31-Check-If-Prime.hs

---
+++ 31-Check-If-Prime.hs
@@ -0,0 +1,14 @@
+isPrime :: Int -> Bool
+isPrime n
+    | n <= 1 = False
+    | n == 2 || n==3 || n==5 = True
+    | n `mod` 2 == 0 = False
+    | otherwise = checkDivision n divisors
+    where endPoint = let f = ((floor $ sqrt $ fromIntegral n)+1) in
+                     if f `mod` 2 == 0 then f+1 else f
+          divisors = [3,5..endPoint]
+          checkDivision n [] = True
+          checkDivision n [x] = n `mod` x /= 0
+          checkDivision n (x:xs)
+              | n `mod` x == 0 = False
+              | otherwise = checkDivision n xs
